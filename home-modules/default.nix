6a3bcd6b8792f6477dd70a038a85bc1cff203a5b
nukdokplex <nukdokplex@nukdokplex.ru>
2025-02-15 11:08:59 +0500

    add hyprland flake to home-manager and nixos both module imports

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -1,7 +1,8 @@
-{ lib, ezModules, inputs, flakeRoot, ... }: {
+{ lib, ezModules, inputs, ... }: {
   imports = lib.attrValues (lib.filterAttrs (name: _: name != "default") ezModules) ++ [
     inputs.spicetify.homeManagerModules.spicetify
     inputs.agenix.homeManagerModules.age
     inputs.nixvim.homeManagerModules.nixvim
+    inputs.hyprland.homeManagerModules.default
   ];
 }
