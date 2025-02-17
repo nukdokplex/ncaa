8647a470bffdd7a73507bcd4c2858434a5c70992
nukdokplex <nukdokplex@nukdokplex.ru>
2025-02-17 19:05:29 +0500

    another impermanence fix

--- nixos-configurations/sleipnir/impermanence.nix
+++ nixos-configurations/sleipnir/impermanence.nix
@@ -5,9 +5,8 @@
 
   programs.fuse.userAllowOther = true;
 
-  environment.persistence."/persistent/system" = {
-    enable = true;
-    hideMounts = true;
+  environment.persistence.main = {
+    persistentStoragePath = "/persistent"; 
     directories = [
       "/var/log"
       "/var/lib/bluetooth"
@@ -18,36 +17,43 @@
     files = [
       "/etc/machine-id"
     ];
+    users.nukdokplex = {
+      directories = [
+        { directory = ".local/state/syncthing"; mode = "0700"; }
+        ".local/state/wireplumber"
+        { directory = ".local/share/TelegramDesktop"; mode = "0700"; }
+        { directory = ".mozilla"; mode = "0700"; }
+        { directory = ".thunderbird"; mode = "0700"; }
+        { directory = ".local/share/Steam"; }
+      ];
+      files = [
+        ".config/mimeapps.list"
+      ];
+    };
   };
 
-  environment.persistence."/persistent/home/nukdokplex".users.nukdokplex = {
-    directories = [
-      { directory = ".local/state/syncthing"; mode = "0700"; }
-      ".local/state/wireplumber"
-      { directory = ".local/share/TelegramDesktop"; mode = "0700"; }
-      { directory = ".mozilla"; mode = "0700"; }
-      { directory = ".thunderbird"; mode = "0700"; }
-      { directory = ".local/share/Steam"; }
+  home-manager.users.nukdokplex = { inputs, ... }: {
+    imports = [
+      inputs.impermanence.homeManagerModules.impermanence
     ];
-    files = [
-      ".config/mimeapps.list"
-    ];
-  };
 
-  environment.persistence."/data/archive/nukdokplex".users.nukdokplex = {
-    directories = [
-      { directory = ".gnupg"; mode = "0700"; }
-      { directory = ".ssh"; mode = "0700"; }
-      "ncaa"
-      "desktop"
-      "documents"
-      "download"
-      "music"
-      "pictures"
-      "publicShare"
-      "templates"
-      "videos"
-    ];
+    home.persistence.archive = {
+      allowOther = true;
+      persistentStoragePath = "/data/archive/nukdokplex"; 
+      directories = [
+        ".ssh"
+        ".gnupg"
+        "ncaa"
+        "desktop"
+        "documents"
+        "download"
+        "music"
+        "pictures"
+        "publicShare"
+        "templates"
+        "videos"
+      ];
+    };
   };
 }
 
