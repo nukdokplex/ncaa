0d7c1ffa1f3953bcbd357084476255766b95a04d
nukdokplex <nukdokplex@nukdokplex.ru>
2025-03-04 00:01:54 +0500

    enable rtkit for common desktops (pipewire dep)

--- nixos-modules/common/desktop/pipewire.nix
+++ nixos-modules/common/desktop/pipewire.nix
@@ -1,5 +1,6 @@
 { lib, config, ... }: {
   config = lib.mkIf config.common.desktop.enable {
+    security.rtkit.enable = lib.mkDefault true; # pipewire uses this to acquire realtime priority
     services.pipewire = {
       enable = true;
       jack.enable = true;
