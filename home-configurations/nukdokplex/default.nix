985630785a862d583c0c8bdd9c3c7a49e5714651
nukdokplex <nukdokplex@nukdokplex.ru>
2025-03-12 18:28:33 +0500

    nukdokplex: add libreoffice-fresh

--- home-configurations/nukdokplex/default.nix
+++ home-configurations/nukdokplex/default.nix
@@ -12,6 +12,7 @@
     stateVersion = "25.05";
     homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${config.home.username}" else "/home/${config.home.username}";
     packages = with pkgs; [
+      libreoffice-fresh
       vesktop
       keepassxc
       ayugram-desktop
