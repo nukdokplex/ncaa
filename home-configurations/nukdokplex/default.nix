fb7c375160f5f60490d00f281240501fb13379cc
nukdokplex <nukdokplex@nukdokplex.ru>
2025-03-13 18:05:31 +0500

    nukdokplex: replace libreoffice with onlyoffice

--- home-configurations/nukdokplex/default.nix
+++ home-configurations/nukdokplex/default.nix
@@ -12,7 +12,7 @@
     stateVersion = "25.05";
     homeDirectory = if pkgs.stdenv.isDarwin then "/Users/${config.home.username}" else "/home/${config.home.username}";
     packages = with pkgs; [
-      libreoffice-fresh
+      onlyoffice-desktopeditors
       vesktop
       keepassxc
       ayugram-desktop
