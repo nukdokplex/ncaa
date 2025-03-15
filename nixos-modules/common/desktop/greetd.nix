12c72eb57d1671dbc09522c108bc871187660e67
nukdokplex <nukdokplex@nukdokplex.ru>
2025-03-15 20:00:08 +0500

    common-desktop@nixos-modules: revert tuigreet

--- nixos-modules/common/desktop/greetd.nix
+++ nixos-modules/common/desktop/greetd.nix
@@ -2,7 +2,7 @@
   config = lib.mkIf config.common.desktop.enable {
     services.greetd = {
       enable = true;
-      vt = 1;
+      vt = 2;
       settings = {
         default_session = {
           command = "${lib.getExe pkgs.greetd.tuigreet} --time --greeting 'Welcome to ${config.networking.hostName}!' --remember --remember-user-session --user-menu";
