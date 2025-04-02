921a52f9fc49c1d80e4fb5c03c176f87176ff4db
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-02 17:33:28 +0500

    common-desktop@nixos-modules: add chromium policies

--- nixos-modules/common/desktop/chromium.nix
+++ nixos-modules/common/desktop/chromium.nix
@@ -1,9 +1,27 @@
-{ pkgs, lib, config, ... }: {
+{ lib, config, ... }: {
   config = lib.mkIf config.common.desktop.enable {
     programs.chromium = {
       enable = true;
-      initialPrefs = {
-
+      extraOpts = {
+        # brave specific
+        # https://support.brave.com/hc/en-us/articles/360039248271-Group-Policy
+        TorDisabled = false;
+        BraveRewardsDisabled = true;
+        BraveWalletDisabled = true;
+        BraveVPNDisabled = 1;
+        BraveAIChatEnabled = false;
+        # common
+        # https://chromeenterprise.google/policies
+        SyncDisabled = true;
+        PasswordManagerEnabled = false;
+        SpellcheckEnabled = true;
+        SpellcheckLanguage = [
+          "en-US"
+          "ru"
+        ];
+        AutofillAddressEnabled = false;
+        AutofillCreditCardEnabled = false;
+        ImportAutofillFormData = false;
       };
     };
   };
