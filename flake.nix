8a924ba2ae585b751954f3eeef3ec8cd57d56dcc
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-03 08:02:58 +0500

    flake.nix: replace flake descriptions with funny @balsoft 's nixos-config flake description

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Nix flake implementing NukDokPlex's NCaA";
+  description = "A collection of crap, hacks, and copy-paste to make my hosts boot";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
