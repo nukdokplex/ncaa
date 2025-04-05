4657dd39c312b1041bf0a204bc61dc68028d0aef
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-05 09:50:57 +0500

    common-desktop@nixos-modules: open yggdrasil multicast port

--- nixos-modules/common-desktop/modules/yggdrasil.nix
+++ nixos-modules/common-desktop/modules/yggdrasil.nix
@@ -2,6 +2,7 @@
   services.yggdrasil = {
     enable = lib.mkDefault true;
     configFile = config.age.secrets.yggdrasil.path;
+    openMulticastPort = true;
   };
 
   age.secrets.yggdrasil.rekeyFile = flakeRoot + /secrets/generated/${config.networking.hostName}/yggdrasil.age;
