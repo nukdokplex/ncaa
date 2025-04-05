b1189516590726bb56eabcb0c4a0e4bd4fc1b90c
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-05 11:04:49 +0500

    gladr@nixos-configurations: fixed tlp

--- nixos-configurations/gladr/modules/power.nix
+++ nixos-configurations/gladr/modules/power.nix
@@ -2,30 +2,8 @@
   services.tlp = {
     enable = true;
     settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-
-      CPU_MIN_PERF_ON_AC = 0;
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
-
-      START_CHARGE_THRESH_BAT0 = 40; # 40 and bellow it starts to charge
-      STOP_CHARGE_THRESH_BAT0 = 80;  # 80 and above it stops charging
-
-      RADEON_DPM_STATE_ON_AC="performance";
-      RADEON_DPM_STATE_ON_BAT="battery";
-      USB_AUTOSUSPEND = 0;
-      USB_EXCLUDE_AUDIO = 1;
-      USB_EXCLUDE_BTUSB = 1;
-      USB_EXCLUDE_PHONE = 1;
-      USB_EXCLUDE_PRINTER = 0;
-      USB_EXCLUDE_WWAN = 1;
+      RUNTIME_PM_ON_BAT = "on";
     };
   };
-  services.upower.enable = true;
-  powerManagement.powertop.enable = true;
+  services.power-profiles-daemon.enable = lib.mkForce false;
 }
