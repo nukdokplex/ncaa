d9a8f0d22282fd697e383b2e3c08140928dfa62a
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-23 11:02:34 +0500

    falhofnir@nixos-configuration: changed wireguard ipv6 network

--- nixos-configurations/falhofnir/modules/wireguard.nix
+++ nixos-configurations/falhofnir/modules/wireguard.nix
@@ -11,20 +11,20 @@ in {
 
     interfaces.${awg0InterfaceName} = {
       type = "amneziawg";
-      ips = [ "10.100.1.1/24" "fe80:10:100:1::1/64" ];
+      ips = [ "10.100.1.1/24" "feee:10:100:1::1/64" ];
       listenPort = awg0Port;
       
       # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
       # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
       postSetup = ''
         '${lib.getExe' pkgs.iptables "iptables"}'  -t nat -A POSTROUTING -s 10.100.1.0/24 -o ${uplink} -j MASQUERADE
-        '${lib.getExe' pkgs.iptables "ip6tables"}' -t nat -A POSTROUTING -s fe80:10:100:1::/64 -o ${uplink} -j MASQUERADE
+        '${lib.getExe' pkgs.iptables "ip6tables"}' -t nat -A POSTROUTING -s feee:10:100:1::/64 -o ${uplink} -j MASQUERADE
       '';
 
       # This undoes the above command
       postShutdown = ''
         '${lib.getExe' pkgs.iptables "iptables"}'  -t nat -D POSTROUTING -s 10.100.1.0/24 -o ${uplink} -j MASQUERADE
-        '${lib.getExe' pkgs.iptables "ip6tables"}' -t nat -D POSTROUTING -s fe80:10:100:1::/64 -o ${uplink} -j MASQUERADE
+        '${lib.getExe' pkgs.iptables "ip6tables"}' -t nat -D POSTROUTING -s feee:10:100:1::/64 -o ${uplink} -j MASQUERADE
       '';
 
       privateKeyFile = config.age.secrets.awg0-private.path;
@@ -33,7 +33,7 @@ in {
         {
           # yggdrasils
           publicKey = "we1V/v3wsZzZknibdQVPyxgMoCgVPvt/5bD2UEoHgVc=";
-          allowedIPs = [ "10.100.1.2/32" "fe80:10:100:1::2/128" ];
+          allowedIPs = [ "10.100.1.2/32" "feee:10:100:1::2/128" ];
         }
       ];
       
