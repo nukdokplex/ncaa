b344bd23a9b136d319ced06395c597581708638c
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-25 11:20:25 +0500

    nukdokplex@home-configurations: didn't expected that librewolf is not saving any login data lol, return to firefox, maybe try librewolf later

--- home-configurations/nukdokplex/modules/librewolf.nix
+++ home-configurations/nukdokplex/modules/librewolf.nix
@@ -1,11 +1,9 @@
 { lib, config, pkgs, ... }: {
   programs.librewolf = {
-    enable = lib.mkDefault config.home.isDesktop;
+    # enable = lib.mkDefault config.home.isDesktop;
     package = pkgs.librewolf;
     settings = {
       
     };
   };
-
-  stylix.targets.librewolf.profileNames = [ "default" ];
 }
