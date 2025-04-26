0fb3a9db000b5c1e4acd7bb84a73c11eec53b24d
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-26 09:37:34 +0500

    nukdokplex@home-configurations: bring zsh back!

--- home-configurations/nukdokplex/modules/shell.nix
+++ home-configurations/nukdokplex/modules/shell.nix
@@ -1,22 +1,28 @@
-{
+{ config, lib, ... }: let
+  flakePath = "path:${lib.escapeShellArg "${config.home.homeDirectory}/ncaa"}";
+  hostName = "$('${lib.getExe' pkgs.nettools "hostname"}')";
+in {
   programs.oh-my-posh = {
     enable = true;
-    enableNushellIntegration = true;
+    enableZshIntegration = true;
     useTheme = "kushal";
   };
-  
-  programs.carapace = {
-    enable = true;
-    enableNushellIntegration = true;
-  };
 
-  programs.nushell = {
+  programs.command-not-found.enable = true;
+  
+  programs.zsh = {
     enable = true;
-shellAliases = {
-      nrs = "sudo nixos-rebuild switch --flake path:/home/nukdokplex/ncaa#(hostname) --accept-flake-config";
-      nrb = "sudo nixos-rebuild boot --flake path:/home/nukdokplex/ncaa#(hostname) --accept-flake-config";
-      nrt = "sudo nixos-rebuild test --flake path:/home/nukdokplex/ncaa#(hostname) --accept-flake-config";
-      nrbuild = "nixos-rebuild build --flake path:/home/nukdokplex/ncaa#(hostname) --accept-flake-config";
+
+    shellAliases = {
+      nrs = "sudo nixos-rebuild --flake ${flakePath}#${hostName} --accept-flake-config switch";
+      nrt = "sudo nixos-rebuild --flake ${flakePath}#${hostName} --accept-flake-config test";
+      nrb = "sudo nixos-rebuild --flake ${flakePath}#${hostName} --accept-flake-config boot";
+      nrbuild = "nixos-rebuild --flake ${flakePath}#${hostName} --accept-flake-config build";
+      agenix-rekey = "nix develop .#agenix-rekey";
+    };
+
+    oh-my-zsh = {
+      enable = true;
     };
   };
 }
