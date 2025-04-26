965f0a27311e7bcaf21a2d25d5c4a8916e9a3b59
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-26 09:36:01 +0500

    default@home-modules: add nix-index-database hm module to imports

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -8,5 +8,6 @@ in {
     inputs.agenix.homeManagerModules.age
     inputs.nixvim.homeManagerModules.nixvim
     inputs.hyprland.homeManagerModules.default
+    inputs.nix-index-database.hmModules.nix-index;
   ];
 }
