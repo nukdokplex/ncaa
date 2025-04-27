99f93f8802b2c16fa5ff8ecdca607ed175d7fb9f
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-27 11:15:16 +0500

    agenix-rekey-generators@nixos-modules: add uuid and reality-keypair secrets generators

--- nixos-modules/agenix-rekey-generators.nix
+++ nixos-modules/agenix-rekey-generators.nix
@@ -1,9 +1,20 @@
 {
-  age.generators.wireguard-priv = {pkgs, lib, file, ...}: let
-    wg = "'${lib.getExe pkgs.wireguard-tools}'";
-  in ''
-    priv=$(${wg} genkey)
-    ${wg} pubkey <<< "$priv" > ${lib.escapeShellArg (lib.removeSuffix ".age" file + ".pub")}
-    echo "$priv"
-  '';
+  age.generators = {
+    wireguard-priv = {pkgs, lib, file, ...}: let
+      wg = "'${lib.getExe pkgs.wireguard-tools}'";
+    in ''
+      priv=$(${wg} genkey)
+      ${wg} pubkey <<< "$priv" > ${lib.escapeShellArg (lib.removeSuffix ".age" file + ".pub")}
+      echo "$priv"
+    '';
+
+    uuid = { pkgs, lib, ... }: "'${lib.getExe' pkgs.util-linux "uuidgen"}'";
+
+    reality-keypair = { pkgs, lib, file, ... }: ''
+      keys=$('${lib.getExe pkgs.sing-box}' generate reality-keypair)
+      echo "$keys" | '${lib.getExe' pkgs.gawk "awk"}' -F ": " '$1 == "PublicKey" { print $2 }' > ${lib.escapeShellArg (lib.removeSuffix ".age" file + ".pub")}
+      echo "$keys" | '${lib.getExe' pkgs.gawk "awk"}' -F ": " '$1 == "PrivateKey" { print $2 }'
+    '';
+  };
+
 }
