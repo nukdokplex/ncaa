c98d67e4e77e4c814ecc811fe6d409691b36610b
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-27 13:00:53 +0500

    agenix-rekey-generators@nixos-modules: add reality-short-id generator

--- nixos-modules/agenix-rekey-generators.nix
+++ nixos-modules/agenix-rekey-generators.nix
@@ -15,6 +15,8 @@
       echo "$keys" | '${lib.getExe' pkgs.gawk "awk"}' -F ": " '$1 == "PublicKey" { print $2 }' > ${lib.escapeShellArg (lib.removeSuffix ".age" file + ".pub")}
       echo "$keys" | '${lib.getExe' pkgs.gawk "awk"}' -F ": " '$1 == "PrivateKey" { print $2 }'
     '';
+
+    reality-short-id = { pkgs, lib, ... }: "'${lib.getExe pkgs.sing-box}' generate rand --hex 8";
   };
 
 }
