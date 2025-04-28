f842f5d25660574c5c660be8b2878a9b09f1ba8e
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-28 19:57:28 +0500

    gler@nixos-configurations: reconfigure network more clear way

--- nixos-configurations/gler/modules/network.nix
+++ nixos-configurations/gler/modules/network.nix
@@ -4,16 +4,39 @@
   lib,
   ...
 }:
+let
+  uplinkMACAddress = "52:54:00:60:44:56";
+in
 {
   networking.useDHCP = false;
-  systemd.network.enable = true;
-  systemd.network.networks."10-uplink" = { };
 
-  # systemd drop-in to keep address secret
-  age.secrets.uplink-address = {
-    path = "/run/systemd/network/10-uplink.network.d/address.conf";
-    rekeyFile = flakeRoot + /secrets/generated/${config.networking.hostName}/uplink-address.age;
-    owner = "systemd-network";
-    group = "systemd-network";
-  };
+  boot.initrd.services.udev.rules = ''
+    SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="${uplinkMACAddress}", NAME="uplink"
+  '';
+
+  systemd.network = lib.fix (self: {
+    enable = true;
+    networks.uplink = {
+      address = [
+        "185.204.3.231/24"
+        "2a04:5200:fff5::1f43/48"
+      ];
+
+      routes = [
+        { Gateway = "185.204.3.1"; }
+        { Gateway = "2a04:5200:fff5::1"; }
+      ];
+
+      dns = [
+        "[2606:4700:4700::1111]:53"
+        "[2606:4700:4700::1001]:53"
+      ];
+
+      matchConfig.MACAddress = uplinkMACAddress;
+      networkConfig = {
+        IPv6AcceptRA = false;
+        LinkLocalAddressing = false;
+      };
+    };
+  });
 }
