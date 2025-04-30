fc3842e494ec237cd42c1fa567afffae6d92385b
nukdokplex <nukdokplex@nukdokplex.ru>
2025-04-30 22:23:47 +0500

    common-base@users: make root users copy authorized ssh keys from nukdokplex user

--- nixos-modules/common-base/modules/users.nix
+++ nixos-modules/common-base/modules/users.nix
@@ -2,29 +2,35 @@
   flakeRoot,
   config,
   pkgs,
+  lib,
   ...
 }:
 {
   programs.zsh.enable = true;
-  users.defaultUserShell = pkgs.zsh;
-  users.mutableUsers = true;
-  users.users.nukdokplex = {
-    name = "nukdokplex";
-    isNormalUser = true;
-    extraGroups = [
-      "wheel"
-      "input"
-      "networkmanager"
-      "adbusers"
-      "cdrom"
-      "podman"
-    ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFX3BPaOblXcbg/frl0PtdJdHp008Pt7N3qqe82+GOSW (none)"
-    ];
+  users = lib.fix (users: {
+    defaultUserShell = pkgs.zsh;
+    mutableUsers = true;
 
-    hashedPasswordFile = config.age.secrets.nukdokplex-password.path;
-  };
+    users.nukdokplex = {
+      name = "nukdokplex";
+      isNormalUser = true;
+      extraGroups = [
+        "wheel"
+        "input"
+        "networkmanager"
+        "adbusers"
+        "cdrom"
+        "podman"
+      ];
+      openssh.authorizedKeys.keys = [
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFX3BPaOblXcbg/frl0PtdJdHp008Pt7N3qqe82+GOSW (none)"
+      ];
+
+      hashedPasswordFile = config.age.secrets.nukdokplex-password.path;
+    };
+
+    users.root.openssh.authorizedKeys.keys = users.users.nukdokplex.openssh.authorizedKeys.keys;
+  });
 
   age.secrets.nukdokplex-password.rekeyFile =
     flakeRoot + /secrets/generated/common/nukdokplex-password.age;
