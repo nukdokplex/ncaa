bb87565c99e950f2615f19d9298d2cf503151d59
nukdokplex <nukdokplex@nukdokplex.ru>
2025-05-01 20:36:41 +0500

    lib: added mail obfuscation and reverse string functions

--- lib/default.nix
+++ lib/default.nix
@@ -1,6 +1,16 @@
 { lib, ... }:
-{
+lib.fix (self: {
   umport = import ./umport.nix { inherit lib; };
   hyprland-utils = import ./hyprland-utils.nix { inherit lib; };
   file-utils = import ./file-utils.nix { inherit lib; };
-}
+
+  reverseString = str: lib.concatStrings (lib.reverseList (lib.stringToCharacters str));
+
+  deobfuscateMail =
+    obfuscatedMail:
+    builtins.concatStringsSep "@" (
+      lib.imap (i: v: (if (lib.mod i 2) == 0 then self.reverseString v else v)) (
+        lib.reverseList (lib.splitString "[has]" (lib.replaceStrings [ "_" ] [ "." ] obfuscatedMail))
+      )
+    );
+})
