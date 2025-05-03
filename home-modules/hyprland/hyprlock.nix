06753d9f38ddb0e39362787ebf1691e46a35bb2c
nukdokplex <nukdokplex@nukdokplex.ru>
2025-05-03 18:46:21 +0500

    hyprland@home-modules: configure hyprlock

--- home-modules/hyprland/hyprlock.nix
+++ home-modules/hyprland/hyprlock.nix
@@ -6,13 +6,37 @@ in
   config = lib.mkIf (cfg.enable && cfg.enableCustomConfiguration) {
     programs.hyprlock = {
       enable = true;
-      settings = {
+      settings = with config.lib.stylix.colors; {
         general = {
           disable_loading_bar = true;
-          grace = 300;
+          grace = 10;
           hide_cursor = true;
           no_fade_in = false;
         };
+
+        input-field = lib.mkForce {
+          rounding = 0;
+          placeholder_text = "<i>$LAYOUT</i>";
+          position = "0, -120";
+          valign = "center";
+          halign = "center";
+          outer_color = "rgb(${base03})";
+          inner_color = "rgb(${base00})";
+          font_color = "rgb(${base05})";
+          fail_color = "rgb(${base08})";
+          check_color = "rgb(${base0A})";
+        };
+
+        label = [
+          {
+            text = "$TIME";
+            font_size = 120;
+            position = "0, 80";
+            valign = "center";
+            halign = "center";
+            color = "rgb(${base05})";
+          }
+        ];
       };
     };
   };
