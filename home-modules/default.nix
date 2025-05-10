a8a65f83d0ae831cee212d017573e1e7a1f0d333
nukdokplex <nukdokplex@nukdokplex.ru>
2025-05-11 03:00:48 +0500

    default@home-modules: use whitelisting instead of blacklisting

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -4,21 +4,18 @@
   inputs,
   ...
 }:
-let
-  excludedModules = [
-    "common"
-  ];
-in
 {
   imports =
-    lib.attrValues (
-      lib.filterAttrs (name: _: !(builtins.elem name (excludedModules ++ [ "default" ]))) ezModules
-    )
-    ++ [
+    [
       inputs.spicetify.homeManagerModules.spicetify
       inputs.agenix.homeManagerModules.age
       inputs.nixvim.homeManagerModules.nixvim
       inputs.hyprland.homeManagerModules.default
       inputs.nix-index-database.hmModules.nix-index
-    ];
+    ]
+    ++ (with ezModules; [
+      gaming
+      is-desktop
+      wayprompt
+    ]);
 }
