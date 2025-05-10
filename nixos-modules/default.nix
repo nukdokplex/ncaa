12d13ec9103efc9631086eb71350344df4919e6d
nukdokplex <nukdokplex@nukdokplex.ru>
2025-05-11 01:52:06 +0500

    default@nixos-modules: use module whitelisting instead of blacklisting

--- nixos-modules/default.nix
+++ nixos-modules/default.nix
@@ -5,30 +5,26 @@
   flakeRoot,
   ...
 }:
-let
-  # here declared module names which should not be imported
-  # notice that "default" module is excluded already
-  excludedModules = [
-    "common-base"
-    "common-desktop"
-    "email-passwords"
-    "default"
-    "dyndns"
-  ];
-in
 {
   # import all modules in this module so this ("default") module become super-module that inherits all nixos modules in this flake
   imports =
-    lib.attrValues (lib.filterAttrs (module: _: !(builtins.elem module excludedModules)) ezModules)
-    ++ [
-      inputs.stylix.nixosModules.stylix
-      inputs.nur.modules.nixos.default
-      inputs.agenix.nixosModules.default
-      inputs.agenix-rekey.nixosModules.default
-      inputs.hyprland.nixosModules.default
-      inputs.disko.nixosModules.disko
-      inputs.tssp.nixosModules.default
-    ];
+    (with inputs; [
+      stylix.nixosModules.stylix
+      nur.modules.nixos.default
+      agenix.nixosModules.default
+      agenix-rekey.nixosModules.default
+      hyprland.nixosModules.default
+      disko.nixosModules.disko
+      tssp.nixosModules.default
+    ])
+    ++ (with ezModules; [
+      k3b-custom
+      lutris
+      optical-disk-essentials
+      steam
+      usb-essentials
+      via
+    ]);
 
   nixpkgs.config = import /${flakeRoot}/nixpkgs-config.nix;
 }
