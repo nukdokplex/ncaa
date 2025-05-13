327489bfc28cdc4f0e0dcdf57b805afc1ebf9ae8
nukdokplex <nukdokplex@nukdokplex.ru>
2025-05-13 07:33:45 +0500

    hyprland@home-modules: improve hyprlock layout

--- home-modules/hyprland/hyprlock.nix
+++ home-modules/hyprland/hyprlock.nix
@@ -1,6 +1,11 @@
-{ lib, config, ... }:
+{ lib, config, ... }@args:
 let
   cfg = config.wayland.windowManager.hyprland;
+  hostNamePath = [
+    "osConfig"
+    "networking"
+    "hostName"
+  ];
 in
 {
   config = lib.mkIf (cfg.enable && cfg.enableCustomConfiguration) {
@@ -16,25 +21,54 @@ in
 
         input-field = lib.mkForce {
           rounding = 0;
-          placeholder_text = "<i>$LAYOUT</i>";
-          position = "0, -120";
+          placeholder_text = "<i>enter password...</i>";
+          position = "0%, 0%";
+          size = "20%, 8%";
           valign = "center";
           halign = "center";
-          outer_color = "rgb(${base03})";
+          outer_color = "rgb(${base0D})";
           inner_color = "rgb(${base00})";
           font_color = "rgb(${base05})";
-          fail_color = "rgb(${base08})";
-          check_color = "rgb(${base0A})";
+          fail_color = "rgb(${red})";
+          check_color = "rgb(${yellow})";
         };
 
         label = [
           {
             text = "$TIME";
+            taxt_align = "left";
             font_size = 120;
-            position = "0, 80";
-            valign = "center";
-            halign = "center";
+            position = "30px, -30px";
+            valign = "top";
+            halign = "left";
+            color = "rgb(${base0D})";
+            shadow_passes = 3;
+          }
+          {
+            text =
+              if lib.hasAttrByPath hostNamePath args then
+                "$USER@${lib.attrByPath hostNamePath "" args}"
+              else
+                "$USER";
+            text_align = "center";
             color = "rgb(${base05})";
+            font_size = 32;
+            position = "0%, 16%";
+            halign = "center";
+            valign = "center";
+            shadow_passes = 3;
+            shadow_boost = 1.4;
+          }
+          {
+            text = "$LAYOUT";
+            text_align = "center";
+            color = "rgb(${base05})";
+            font_size = 16;
+            position = "0%, 10%";
+            halign = "center";
+            valign = "center";
+            shadow_passes = 3;
+            shadow_boost = 1.4;
           }
         ];
       };
