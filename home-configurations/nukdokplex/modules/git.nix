a9b3ee61ef860a8109dd2b6225776fe41902472f
nukdokplex <nukdokplex@nukdokplex.ru>
2025-05-13 07:47:36 +0500

    nukdokplex@home-configurations: improve git signing configuration

--- home-configurations/nukdokplex/modules/git.nix
+++ home-configurations/nukdokplex/modules/git.nix
@@ -1,11 +1,13 @@
+{ lib, ... }:
 {
-  programs.git = {
+  programs.git = lib.fix (git: {
     enable = true;
+    userName = "nukdokplex";
+    userEmail = "nukdokplex@nukdokplex.ru";
     signing = {
       format = "openpgp";
       signByDefault = true;
+      key = git.userEmail;
     };
-    userName = "nukdokplex";
-    userEmail = "nukdokplex@nukdokplex.ru";
-  };
+  });
 }
