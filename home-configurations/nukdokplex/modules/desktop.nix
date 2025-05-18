4efd1a7ccd7af685113d563656ab83fe796ce779
nukdokplex <nukdokplex@nukdokplex.ru>
2025-05-19 00:45:20 +0500

    nukdokplex@home-configurations: add yubioath-flutter, openpgp-card-tools and pico-fido-tool to desktop related packages

--- home-configurations/nukdokplex/modules/desktop.nix
+++ home-configurations/nukdokplex/modules/desktop.nix
@@ -2,25 +2,35 @@
   pkgs,
   lib,
   config,
+  inputs,
   ...
 }:
 {
   config = lib.mkIf config.home.isDesktop {
     # there we add only desktop stuff
-    home.packages = with pkgs; [
-      onlyoffice-desktopeditors
-      vesktop
-      keepassxc
-      ayugram-desktop
-      qbittorrent
-      vlc
-      tor-browser
-      chromium
-      font-manager
-      gimp
-      shotwell
-      yubioath-flutter
-    ];
+    home.packages =
+      with pkgs;
+      [
+        onlyoffice-desktopeditors
+        vesktop
+        keepassxc
+        ayugram-desktop
+        qbittorrent
+        vlc
+        tor-browser
+        chromium
+        font-manager
+        gimp
+        shotwell
+
+        # hw keys
+        yubioath-flutter
+        openpgp-card-tools
+      ]
+      ++ lib.optional (
+        pkgs.system == "x86_64-linux"
+      ) inputs.picokeys-nix.packages.x86_64-linux.pico-fido-tool;
+
     services = {
       syncthing.enable = true;
     };
