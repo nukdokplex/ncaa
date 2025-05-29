c52c0b8d65a2c1b3beadeb0ac15c13523a07edf7
nukdokplex <nukdokplex@nukdokplex.ru>
2025-05-29 08:13:26 +0500

    nemo@home-modules: add final package

--- home-modules/nemo.nix
+++ home-modules/nemo.nix
@@ -10,7 +10,18 @@ in
 {
   options.programs.nemo = {
     enable = lib.mkEnableOption "Nemo, the file manager from Cinnamon";
-    package = lib.mkPackageOption pkgs "nemo" { };
+    package = lib.mkPackageOption pkgs "nemo" {
+      extraDescription = "Don't use it to get executable! Use `config.programs.nemo.finalPackage` instead.";
+    };
+    finalPackage = lib.mkOption {
+      description = "Final nemo package with extensions, use it to get executable.";
+      type = lib.types.package;
+      default = pkgs.nemo-with-extensions.override {
+        nemo = cfg.package;
+        extensions = cfg.extensions;
+      };
+      readOnly = true;
+    };
     extensions = lib.mkOption {
       default = [ ];
       defaultText = "[ ]";
@@ -21,12 +32,7 @@ in
   };
 
   config = lib.mkIf cfg.enable {
-    home.packages = lib.singleton (
-      pkgs.nemo-with-extensions.override {
-        nemo = cfg.package;
-        extensions = cfg.extensions;
-        useDefaultExtensions = true;
-      }
-    );
+    home.packages = lib.singleton cfg.finalPackage;
+    dbus.packages = lib.singleton cfg.finalPackage;
   };
 }
