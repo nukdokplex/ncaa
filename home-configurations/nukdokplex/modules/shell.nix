83927c8bea3321add197d691f004e5327e20c1ed
nukdokplex <nukdokplex@nukdokplex.ru>
2025-06-16 23:17:35 +0500

    nukdokplex@home-configurations: improved shell configuration

--- home-configurations/nukdokplex/modules/shell.nix
+++ home-configurations/nukdokplex/modules/shell.nix
@@ -6,45 +6,44 @@
 }:
 let
   flakePath = "path:${lib.escapeShellArg "${config.home.homeDirectory}/ncaa"}";
-  hostName = "$('${lib.getExe' pkgs.nettools "hostname"}')";
 in
 {
-  programs.oh-my-posh = {
-    enable = true;
-    enableZshIntegration = true;
-    useTheme = "kushal";
-  };
-
-  programs.nix-index = {
-    enable = true;
-  };
-
-  programs.zsh = {
-    enable = true;
-
-    initContent = lib.mkBefore ''
-      ZSH_TMUX_AUTOSTART=true
-      if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
-        ZSH_TMUX_AUTOSTART=false
-      fi
-    '';
-
-    sessionVariables = {
-      ZSH_TMUX_AUTOCONNECT = "true";
-      ZSH_TMUX_AUTOQUIT = "false";
-    };
-
-    shellAliases = {
-      nrs = "sudo nixos-rebuild --flake ${flakePath}#${hostName} --accept-flake-config switch";
-      nrt = "sudo nixos-rebuild --flake ${flakePath}#${hostName} --accept-flake-config test";
-      nrb = "sudo nixos-rebuild --flake ${flakePath}#${hostName} --accept-flake-config boot";
-      nrbuild = "nixos-rebuild --flake ${flakePath}#${hostName} --accept-flake-config build";
-      agenix-rekey = "nix develop .#agenix-rekey";
-    };
-
-    oh-my-zsh = {
+  programs = {
+    direnv.enable = true;
+    nix-index.enable = true;
+    oh-my-posh = {
       enable = true;
-      plugins = [ "tmux" ];
+      useTheme = "kushal";
+    };
+
+    zsh = {
+      enable = true;
+
+      initContent = lib.mkBefore ''
+        ZSH_TMUX_AUTOSTART=true
+
+        # disable tmux autostart when ssh client is connected
+        if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+          ZSH_TMUX_AUTOSTART=false
+        fi
+      '';
+
+      sessionVariables = {
+        ZSH_TMUX_AUTOCONNECT = "true";
+        ZSH_TMUX_AUTOQUIT = "false";
+      };
+
+      shellAliases = {
+        nrs = "sudo nixos-rebuild --flake ${flakePath}#$('${lib.getExe' pkgs.nettools "hostname"}') --accept-flake-config switch";
+        nrt = "sudo nixos-rebuild --flake ${flakePath}#$('${lib.getExe' pkgs.nettools "hostname"}') --accept-flake-config test";
+        nrb = "sudo nixos-rebuild --flake ${flakePath}#$('${lib.getExe' pkgs.nettools "hostname"}') --accept-flake-config boot";
+        nrbuild = "nixos-rebuild --flake  ${flakePath}#$('${lib.getExe' pkgs.nettools "hostname"}') --accept-flake-config build";
+      };
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "tmux" ];
+      };
     };
   };
 }
