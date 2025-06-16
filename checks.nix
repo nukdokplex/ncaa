d1c52cc1a627d7e8cb623cddfe7154fb9045de4d
nukdokplex <nukdokplex@nukdokplex.ru>
2025-06-16 18:52:52 +0500

    checks: replaced treefmt with nixfmt-rfc-style hook in pre-commit-check

--- checks.nix
+++ checks.nix
@@ -11,14 +11,7 @@
       checks.pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
         src = flakeRoot;
         hooks = {
-          treefmt = {
-            enable = true;
-            settings = {
-              fail-on-change = true;
-              no-cache = true;
-              formatters = [ config.formatter ];
-            };
-          };
+          nixfmt-rfc-style.enable = true;
         };
       };
     };
