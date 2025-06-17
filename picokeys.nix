1cd9baf51f77e132e38b1a35cd6299acfdbffa4f
nukdokplex <nukdokplex@nukdokplex.ru>
2025-06-17 23:23:12 +0500

    picokeys: added pico-fido2, improved configurations of other packages

--- picokeys.nix
+++ picokeys.nix
@@ -15,36 +15,27 @@ in
       picokeys-pkgs = inputs.picokeys-nix.packages.${system};
     in
     {
-      pico-fido =
-        (picokeys-pkgs.pico-fido.overrideAttrs (
-          final: prev: {
-            cmakeFlags = lib.concatLists [
-              prev.cmakeFlags
-              (lib.singleton (lib.cmakeBool "ENABLE_DELAYED_BOOT" true))
-            ];
-          }
-        )).override
-          {
-            picoBoard = "waveshare_rp2350_one";
-            vidPid = "Yubikey5";
-            eddsaSupport = true;
-            generateOtpFile = true;
-          };
-      pico-openpgp =
-        (picokeys-pkgs.pico-openpgp.overrideAttrs (
-          final: prev: {
-            cmakeFlags = lib.concatLists [
-              prev.cmakeFlags
-              (lib.singleton (lib.cmakeBool "ENABLE_DELAYED_BOOT" true))
-            ];
-          }
-        )).override
-          {
-            picoBoard = "waveshare_rp2350_one";
-            vidPid = "Yubikey5";
-            eddsaSupport = true;
-            generateOtpFile = true;
-          };
+      pico-fido = picokeys-pkgs.pico-fido.override {
+        picoBoard = "waveshare_rp2350_one";
+        vidPid = "Yubikey5";
+        delayedBoot = true;
+        eddsaSupport = true;
+        generateOtpFile = true;
+      };
+      pico-openpgp = picokeys-pkgs.pico-openpgp.override {
+        picoBoard = "waveshare_rp2350_one";
+        vidPid = "Yubikey5";
+        delayedBoot = true;
+        eddsaSupport = true;
+        generateOtpFile = true;
+      };
+      pico-fido2 = picokeys-pkgs.pico-fido2.override {
+        picoBoard = "waveshare_rp2350_one";
+        vidPid = "Yubikey5";
+        delayedBoot = true;
+        eddsaSupport = true;
+        generateOtpFile = true;
+      };
       pico-nuke = picokeys-pkgs.pico-nuke.override {
         picoBoard = "waveshare_rp2350_one";
         generateOtpFile = true;
