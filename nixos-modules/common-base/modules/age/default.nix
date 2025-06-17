955ae539e8001a65aab6eab16a4d668bf0625ce4
nukdokplex <nukdokplex@nukdokplex.ru>
2025-06-18 03:49:30 +0500

    secrets, common-base@nixos-modules: added fido2 master identity to agenix, removed redundant master identities, rekeyed secrets

--- nixos-modules/common-base/modules/age/default.nix
+++ nixos-modules/common-base/modules/age/default.nix
@@ -16,10 +16,6 @@
         identity = ./pico-fido-1.pub;
         pubkey = "age10crup2cf6jkhytl0xm03wns6heewn3rvxaan7jp85990mwwas3fs8392z9";
       }
-      {
-        identity = ./openpgp-card-1.pub;
-        pubkey = "age1q87qln93vs9g9x4zkx2cc4v59duykng0xqdzjtjvs43qc6qqkujsq4kv3f";
-      }
     ];
     extraEncryptionPubkeys = [
       "age1uhv9l24d4rnrxtydm4mvmmh2653x22ae0ysmhgejhmrtsr8a6e6qnh29vf"
