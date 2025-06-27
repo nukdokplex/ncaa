87cfd0af42ae06d6aa7bad9ab82d3b466a8e4430
nukdokplex <nukdokplex@nukdokplex.ru>
2025-06-27 08:52:33 +0500

    common-desktop@nixos-modules: add some gstreamer plugins and codecs

--- nixos-modules/common-desktop/graphics.nix
+++ nixos-modules/common-desktop/graphics.nix
@@ -1,7 +1,26 @@
-{ lib, config, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 {
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
+    extraPackages = with pkgs; [
+      libva-vdpau-driver
+      libvdpau-va-gl
+    ];
   };
+
+  environment.systemPackages = with pkgs; [
+    gst_all_1.gstreamer
+    gst_all_1.gst-libav
+    gst_all_1.gst-vaapi
+    gst_all_1.gst-plugins-bad
+    gst_all_1.gst-plugins-ugly
+    gst_all_1.gst-plugins-good
+    gst_all_1.gst-plugins-base
+  ];
 }
