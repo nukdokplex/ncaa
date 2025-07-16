55fb542fdc637aceda5b7d39dfccf68bf3e5bb8d
nukdokplex <nukdokplex@nukdokplex.ru>
2025-07-16 18:39:49 +0500

    fix xdg-open is not opening foot for terminal desktop files

--- home-configurations/nukdokplex/foot.nix
+++ home-configurations/nukdokplex/foot.nix
@@ -1,20 +1,36 @@
-{ lib, config, ... }:
 {
-  programs.foot = {
-    enable = lib.mkDefault config.home.isDesktop;
-    settings = {
-      mouse = {
-        hide-when-typing = "yes";
-      };
-
-      key-bindings =
-        let
-          mod = "Mod1";
-        in
-        {
-          clipboard-paste = "Control+Shift+v XF86Paste ${mod}+P";
-          clipboard-copy = "Control+Shift+c XF86Copy ${mod}+Y";
+  lib,
+  config,
+  pkgs,
+  ...
+}:
+{
+  config = lib.mkIf config.home.isDesktop {
+    programs.foot = {
+      enable = true;
+      settings = {
+        mouse = {
+          hide-when-typing = "yes";
         };
+
+        key-bindings =
+          let
+            mod = "Mod1";
+          in
+          {
+            clipboard-paste = "Control+Shift+v XF86Paste ${mod}+P";
+            clipboard-copy = "Control+Shift+c XF86Copy ${mod}+Y";
+          };
+      };
     };
+    home.sessionVariables.TERMINAL = "foot";
+
+    # workaround for .desktop files with Terminal=true
+    # xdg-open has a hardcoded list of terminals
+    xdg.configFile."xdg-terminals.list".text = ''
+      foot.desktop
+    '';
+
+    xdg.dataFile."xdg-terminals/foot.desktop".source = "${pkgs.foot}/share/applications/foot.desktop";
   };
 }
