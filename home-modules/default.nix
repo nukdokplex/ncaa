27ece1e1b9a6e3ea9a0ad5f9acfeb920a5c4758b
nukdokplex <nukdokplex@nukdokplex.ru>
2025-07-22 11:54:45 +0500

    nix-index-database.homeModules instead of hmModules
    change due to nix-community/nix-index-database#154

--- home-modules/default.nix
+++ home-modules/default.nix
@@ -5,23 +5,22 @@
   ...
 }:
 {
-  imports =
-    [
-      inputs.spicetify.homeManagerModules.spicetify
-      inputs.agenix.homeManagerModules.age
-      inputs.hyprland.homeManagerModules.default
-      inputs.nix-index-database.hmModules.nix-index
-      inputs.nixcord.homeModules.nixcord
-    ]
-    ++
-      # this is workaround to make stylix always been imported and prevent config conflicts
-      (lib.optional (!osConfig.stylix.enable) inputs.stylix.homeManagerModules.stylix)
-    ++ [
-      ./file-roller.nix
-      ./gaming.nix
-      ./is-desktop.nix
-      ./nemo.nix
-      ./nm-applet.nix
-      ./wm-settings.nix
-    ];
+  imports = [
+    inputs.spicetify.homeManagerModules.spicetify
+    inputs.agenix.homeManagerModules.age
+    inputs.hyprland.homeManagerModules.default
+    inputs.nix-index-database.homeModules.nix-index
+    inputs.nixcord.homeModules.nixcord
+  ]
+  ++
+    # this is workaround to make stylix always been imported and prevent config conflicts
+    (lib.optional (!osConfig.stylix.enable) inputs.stylix.homeManagerModules.stylix)
+  ++ [
+    ./file-roller.nix
+    ./gaming.nix
+    ./is-desktop.nix
+    ./nemo.nix
+    ./nm-applet.nix
+    ./wm-settings.nix
+  ];
 }
