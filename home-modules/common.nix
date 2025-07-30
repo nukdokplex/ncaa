434deaf61f6581b23314cd2f23f8c8b0d2dc245f
nukdokplex <nukdokplex@nukdokplex.ru>
2025-07-31 03:32:25 +0500

    make qt use xdg-desktop-portal for system dialogs

--- home-modules/common.nix
+++ home-modules/common.nix
@@ -1,4 +1,9 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  lib,
+  ...
+}:
 {
 
   stylix.iconTheme = {
@@ -11,4 +16,16 @@
   stylix.targets.gtk.extraCss = with config.lib.stylix; ''
     @define-color view_bg_color rgba(${colors.base00-rgb-r}, ${colors.base00-rgb-g}, ${colors.base00-rgb-b}, ${toString config.stylix.opacity.applications});
   '';
+
+  xdg.configFile =
+    let
+      qtctCfg = ''
+        standard_dialogs=xdgdesktopportal
+      '';
+    in
+    {
+      # addition to stylix qtct configuration
+      "qt5ct/qt5ct.conf".text = lib.mkOrder 1050 qtctCfg;
+      "qt6ct/qt6ct.conf".text = lib.mkOrder 1050 qtctCfg;
+    };
 }
