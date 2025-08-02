abff8283759e3e124410949c871a97f983f8e7ca
nukdokplex <nukdokplex@nukdokplex.ru>
2025-08-02 21:54:23 +0500

    greetd is now fixed to TTY1
    thx NixOS/nixpkgs#428972

--- nixos-modules/common-desktop/greetd.nix
+++ nixos-modules/common-desktop/greetd.nix
@@ -7,7 +7,6 @@
 {
   services.greetd = {
     enable = true;
-    vt = 2;
     settings = {
       default_session = {
         command = "${lib.getExe pkgs.greetd.tuigreet} --time --greeting 'Welcome to ${config.networking.hostName}!' --remember --remember-user-session --user-menu";
