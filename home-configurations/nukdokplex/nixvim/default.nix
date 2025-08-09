bcc0d310bfc842499d4bacae0843f39ee9a7dc86
nukdokplex <nukdokplex@nukdokplex.ru>
2025-08-09 20:57:31 +0500

    nixvim transparent background

--- home-configurations/nukdokplex/nixvim/default.nix
+++ home-configurations/nukdokplex/nixvim/default.nix
@@ -15,4 +15,5 @@
 
   programs.nixvim.enable = lib.mkIf config.home.isDesktop true;
   programs.nixvim.nixpkgs.useGlobalPackages = true;
+  stylix.targets.nixvim.transparentBackground.main = true;
 }
