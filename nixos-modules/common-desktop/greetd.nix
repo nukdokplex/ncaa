0e0f9d95de22474f5e497f524b99fe5f7e584b68
nukdokplex <nukdokplex@nukdokplex.ru>
2025-08-10 11:58:00 +0500

    fix tuigreet's output being overwrited by systemd journal
    see discussion:
    https://github.com/apognu/tuigreet/issues/190#issuecomment-3157730941

--- nixos-modules/common-desktop/greetd.nix
+++ nixos-modules/common-desktop/greetd.nix
@@ -13,4 +13,15 @@
       };
     };
   };
+
+  systemd.services.greetd.serviceConfig = {
+    Type = "idle";
+    StandardInput = "tty";
+    StandardOutput = "tty";
+    StandardError = "journal"; # Without this errors will spam on screen
+    # Without these bootlogs will spam on screen
+    TTYReset = true;
+    TTYVHangup = true;
+    TTYVTDisallocate = true;
+  };
 }
