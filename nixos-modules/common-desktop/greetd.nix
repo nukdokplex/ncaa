ea7b0fc27586eb06111694057bd79e362f07eab9
nukdokplex <nukdokplex@nukdokplex.ru>
2025-08-10 11:29:32 +0500

    fix greetd.tuigreet was moved to tuigreet
    due to NixOS/nixpkgs#427540

--- nixos-modules/common-desktop/greetd.nix
+++ nixos-modules/common-desktop/greetd.nix
@@ -9,7 +9,7 @@
     enable = true;
     settings = {
       default_session = {
-        command = "${lib.getExe pkgs.greetd.tuigreet} --time --greeting 'Welcome to ${config.networking.hostName}!' --remember --remember-user-session --user-menu";
+        command = "${lib.getExe pkgs.tuigreet} --time --greeting 'Welcome to ${config.networking.hostName}!' --remember --remember-user-session --user-menu";
       };
     };
   };
