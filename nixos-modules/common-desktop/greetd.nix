e01ea5e29b3ce52d9e5b6587f7ea58c6f0691727
nukdokplex <nukdokplex@nukdokplex.ru>
2025-08-28 06:10:27 +0500

    greetd: use "useTextGreeter" option instead of manually tweaking greetd systemd service
    introduced in https://github.com/NixOS/nixpkgs/pull/433962

--- nixos-modules/common-desktop/greetd.nix
+++ nixos-modules/common-desktop/greetd.nix
@@ -7,21 +7,11 @@
 {
   services.greetd = {
     enable = true;
+    useTextGreeter = true;
     settings = {
       default_session = {
         command = "${lib.getExe pkgs.tuigreet} --time --greeting 'Welcome to ${config.networking.hostName}!' --remember --remember-user-session --user-menu";
       };
     };
-  };
-
-  systemd.services.greetd.serviceConfig = {
-    Type = "idle";
-    StandardInput = "tty";
-    StandardOutput = "tty";
-    StandardError = "journal"; # Without this errors will spam on screen
-    # Without these bootlogs will spam on screen
-    TTYReset = true;
-    TTYVHangup = true;
-    TTYVTDisallocate = true;
   };
 }
