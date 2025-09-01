45ec6855151606e5e51c0a9a94b71b0c5178591f
nukdokplex <nukdokplex@nukdokplex.ru>
2025-09-02 03:29:37 +0500

    holl: added some graphics and fixed jellyfin hardware acceleration

---
+++ nixos-configurations/holl/graphics.nix
@@ -0,0 +1,28 @@
+{ pkgs, ... }:
+{
+  hardware.graphics = {
+    enable = true;
+    extraPackages = with pkgs; [
+      libva-vdpau-driver
+      intel-ocl
+    ];
+  };
+
+  systemd.services.jellyfin.environment.LIBVA_DRIVER_NAME = "iHD";
+  environment.sessionVariables.LIBVA_DRIVER_NAME = "iHD";
+
+  nixpkgs.overlays = [
+    (_: prev: {
+      jellyfin-ffmpeg = prev.jellyfin-ffmpeg.override {
+        # Exact version of ffmpeg_* depends on what jellyfin-ffmpeg package is using.
+        # In 24.11 it's ffmpeg_7-full.
+        # See jellyfin-ffmpeg package source for details
+        ffmpeg_7-full = prev.ffmpeg_7-full.override {
+          withMfx = false; # This corresponds to the older media driver
+          withVpl = true; # This is the new driver
+          withUnfree = true;
+        };
+      };
+    })
+  ];
+}
