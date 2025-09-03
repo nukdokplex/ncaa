3dfec636adc6a7c5c9ebcd75b4bfbd0ca14e5547
nukdokplex <nukdokplex@nukdokplex.ru>
2025-09-04 00:20:00 +0500

    sleipnir: config refinement
    * use nixos-hardware to define host hardware;
    
    * added nct6687d kernel module;
    
    * correct network devices naming;
    
    * code cleanup;

---
+++ nixos-configurations/sleipnir/hardware.nix
@@ -0,0 +1,43 @@
+{
+  lib,
+  pkgs,
+  inputs,
+  config,
+  ...
+}:
+{
+  nixpkgs.localSystem.system = "x86_64-linux";
+
+  imports = [
+    (inputs.nixos-hardware + /common/cpu/amd)
+    (inputs.nixos-hardware + /common/gpu/amd)
+    (inputs.nixos-hardware + /common/pc)
+    (inputs.nixos-hardware + /common/pc/ssd)
+  ];
+
+  hardware.enableRedistributableFirmware = true;
+
+  boot.extraModulePackages = with config.boot.kernelPackages; [
+    nct6687d # MSI MAG B550 TOMAHAWK (MS-7C91) eSIO chip Nuvoton NCT66875-R
+  ];
+
+  boot.kernelModules = [ "nct6687d" ];
+
+  # gpu staff
+  nixpkgs.config.rocmSupport = true; # AMDGPU support for packages
+
+  hardware.graphics.extraPackages = with pkgs; [
+    rocmPackages.clr.icd
+  ];
+
+  systemd.packages = with pkgs; [ lact ];
+  systemd.services.lactd.wantedBy = [ "multi-user.target" ];
+
+  environment.systemPackages = with pkgs; [
+    lact
+  ];
+  # gpu staff
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = lib.mkIf config.hardware.bluetooth.enable true;
+}
--- nixos-configurations/sleipnir/network.nix
+++ nixos-configurations/sleipnir/network.nix
@@ -1,3 +1,12 @@
 {
-  networking.nftables.firewall.rules.nixos-firewall.from = [ "uplink" ];
+  networking.nftables.firewall.rules.nixos-firewall.from = [ "uplink*" ];
+
+  networking.interfaces.uplink0.wakeOnLan.enable = true;
+  networking.interfaces.uplink1.wakeOnLan.enable = true;
+
+  services.udev.extraRules = ''
+    SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="d8:43:ae:95:44:e7", NAME="uplink0"
+    SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="d8:43:ae:95:44:e8", NAME="uplink1"
+  '';
+
 }
