93ea0f97935da22954d8aa0fbb18af9dc41e358b
nukdokplex <nukdokplex@nukdokplex.ru>
2025-09-04 01:56:15 +0500

    gler: moved hardware config to separate module

--- nixos-configurations/gler/default.nix
+++ nixos-configurations/gler/default.nix
@@ -3,7 +3,6 @@
   lib,
   ezModules,
   inputs,
-  modulesPath,
   ...
 }:
 {
@@ -17,13 +16,10 @@
       ezModules.common-base
       ezModules.acme
       inputs.simple-nixos-mailserver.nixosModule
-      (modulesPath + "/profiles/qemu-guest.nix") # adds virtio and 9p kernel modules
     ];
 
-  nixpkgs.localSystem.system = "x86_64-linux";
   time.timeZone = "Europe/Moscow";
   system.stateVersion = "25.11";
-  hardware.enableAllFirmware = true;
 
   age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFnNGUSphRtp3azpcSzvO2Tlm75fmn0YbyEzB9Oa6sLb root@gler";
 
---
+++ nixos-configurations/gler/hardware.nix
@@ -0,0 +1,10 @@
+{ modulesPath, ... }:
+{
+  nixpkgs.localSystem.system = "x86_64-linux";
+
+  imports = [
+    (modulesPath + /profiles/qemu-guest.nix) # adds virtio and 9p kernel modules
+  ];
+
+  hardware.enableRedistributableFirmware = false;
+}
