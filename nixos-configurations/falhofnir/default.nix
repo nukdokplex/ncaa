fe799190f54bda97736d82058bbeabca60ce13cc
nukdokplex <nukdokplex@nukdokplex.ru>
2025-09-04 05:21:44 +0500

    falhofnir: split hardware configuration file into separate module

--- nixos-configurations/falhofnir/default.nix
+++ nixos-configurations/falhofnir/default.nix
@@ -3,7 +3,6 @@
   lib,
   ezModules,
   inputs,
-  modulesPath,
   ...
 }:
 {
@@ -17,13 +16,10 @@
       ezModules.common-base
       ezModules.acme
       inputs.simple-nixos-mailserver.nixosModule
-      (modulesPath + "/profiles/qemu-guest.nix") # adds virtio and 9p kernel modules
     ];
 
-  nixpkgs.localSystem.system = "x86_64-linux";
   time.timeZone = "Europe/Amsterdam";
   system.stateVersion = "25.05";
-  hardware.enableAllFirmware = true;
 
   age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKe9E7Kw34WZQl7T0MluQd3tKvdhNvEcvDJa9Zr3hJWv root@falhofnir";
 
