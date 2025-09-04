1d715d788ec6282e9938eb372ef6ef7f9663ab51
nukdokplex <nukdokplex@nukdokplex.ru>
2025-09-04 06:25:24 +0500

    falhofnir, gler: fix modulesPath use

--- nixos-configurations/gler/hardware.nix
+++ nixos-configurations/gler/hardware.nix
@@ -3,7 +3,7 @@
   nixpkgs.localSystem.system = "x86_64-linux";
 
   imports = [
-    (modulesPath + /profiles/qemu-guest.nix) # adds virtio and 9p kernel modules
+    (modulesPath + "/profiles/qemu-guest.nix") # adds virtio and 9p kernel modules
   ];
 
   hardware.enableRedistributableFirmware = false;
