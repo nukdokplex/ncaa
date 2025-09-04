979337b1c63e1a0b6021a11d15358f621eeb0baf
nukdokplex <nukdokplex@nukdokplex.ru>
2025-09-04 05:12:19 +0500

    gladr: config refinement
    * improved power config;
    * configuration clean;
    * hardware defined through nixos-hardware;

--- nixos-configurations/gladr/default.nix
+++ nixos-configurations/gladr/default.nix
@@ -1,8 +1,6 @@
 {
-  lib,
   lib',
   ezModules,
-  config,
   ...
 }:
 {
@@ -23,14 +21,10 @@
       netbird-client
     ]);
 
-  nixpkgs.localSystem.system = "x86_64-linux";
-  nixpkgs.config.rocmSupport = true; # AMDGPU support for packages
   time.timeZone = "Asia/Yekaterinburg";
   system.stateVersion = "25.05";
   hardware.enableAllFirmware = true;
 
   age.rekey.hostPubkey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDo2pi5x42hS1jbB9gHvMfr3iiDWr4Mpe5CPNhpddIGH root@gladr";
 
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = lib.mkIf config.hardware.bluetooth.enable true;
 }
---
+++ nixos-configurations/gladr/hardware.nix
@@ -0,0 +1,58 @@
+{
+  lib,
+  inputs,
+  config,
+  ...
+}:
+{
+  nixpkgs.localSystem.system = "x86_64-linux";
+
+  imports = [
+    (inputs.nixos-hardware + /common/cpu/amd)
+    (inputs.nixos-hardware + /common/cpu/amd/pstate.nix)
+    (inputs.nixos-hardware + /common/cpu/amd/zenpower.nix)
+    (inputs.nixos-hardware + /common/gpu/amd)
+    (inputs.nixos-hardware + /common/pc/laptop)
+    (inputs.nixos-hardware + /common/pc/laptop/hdd)
+    (inputs.nixos-hardware + /common/pc/ssd)
+  ];
+
+  hardware.enableRedistributableFirmware = true;
+
+  services.fprintd.enable = true;
+
+  services.logind.settings.Login = {
+    HandlePowerKey = "poweroff";
+    HandleRebootKey = "reboot";
+    HandleSuspendKey = "suspend-then-hibernate";
+    HandleSuspendKeyLongPress = "suspend";
+    HandleHibernateKey = "hibernate";
+    HandleLidSwitch = "suspend-then-hibernate";
+    HandleLidSwitchExternalPower = "lock";
+    HandleLidSwitchDocked = "ignore";
+    HandleSecureAttentionKey = "lock";
+  };
+
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=yes
+    AllowHibernation=yes
+    AllowHybridSleep=no
+    AllowSuspendThenHibernate=yes
+
+    # SuspendState=
+    # HibernateMode=
+    # MemorySleepMode=
+
+    HibernateOnACPower=no
+
+    HibernateDelaySec=30min
+    SuspendEstimationSec=10min
+  '';
+
+  powerManagement.powertop.enable = true;
+
+  nixpkgs.config.rocmSupport = true; # AMDGPU support for packages
+
+  hardware.bluetooth.enable = true;
+  services.blueman.enable = lib.mkIf config.hardware.bluetooth.enable true;
+}
