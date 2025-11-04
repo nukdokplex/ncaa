16f899da29cb69fa8080d7399b006c4fd7cfacb6
nukdokplex <nukdokplex@nukdokplex.ru>
2025-11-04 19:18:43 +0500

    nixos-configurations/holl: replace epson_201310w driver with epson_201207w
    l110 printer drivers are compatible with l120 drivers and they are not
    broken

--- nixos-configurations/holl/printing.nix
+++ nixos-configurations/holl/printing.nix
@@ -1,13 +1,15 @@
 { pkgs, ... }:
 {
-  services.printing.drivers = [ pkgs.epson_201310w ];
+  services.printing.enable = true;
+  services.printing.drivers = with pkgs; [ epson_201207w ];
 
   hardware.printers.ensurePrinters = [
     {
       name = "Epson_L120_Series";
       location = "Home";
       deviceUri = "usb://EPSON/L120%20Series?serial=544E594B3132383744";
-      model = "EPSON_L120.ppd";
+      model = "epson-inkjet-printer-201207w/ppds/EPSON_L110.ppd";
+      # l110 printer drivers are compatible with l120 drvers just because they are using same filter program. also l110 drivers bring a bit more types of paper available than l120
     }
   ];
 }
