af15751ca0302919768202ba81745bb2d6debaf2
nukdokplex <nukdokplex@nukdokplex.ru>
2025-11-04 19:33:10 +0500

    home-configurations/nukdokplex: use iosevka as foot default font

--- home-configurations/nukdokplex/foot.nix
+++ home-configurations/nukdokplex/foot.nix
@@ -8,6 +8,7 @@
     programs.foot = {
       enable = true;
       settings = {
+        main.font = "IosevkaTerm Nerd Font:size=${toString (config.stylix.fonts.sizes.terminal or 12)}";
         mouse = {
           hide-when-typing = "yes";
         };
