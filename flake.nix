bb21cc7cd63757818d7f223ace32ffa5427869d3
nukdokplex <nukdokplex@nukdokplex.ru>
2025-11-22 01:25:16 +0500

    flake: remove nukdokplex.cachix.org from substituters

--- flake.nix
+++ flake.nix
@@ -66,12 +66,10 @@
       "pipe-operators"
     ];
     extra-substituters = [
-      "https://nukdokplex.cachix.org"
       "https://nix-community.cachix.org"
       "https://hyprland.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "nukdokplex.cachix.org-1:ikhOYrqfHkiYDCfnYKucldBri13hBtfC+8bTBTT7hW0="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
     ];
