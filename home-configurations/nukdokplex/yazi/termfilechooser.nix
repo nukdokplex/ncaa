79c8ea7c1e0c478970a53da3dbbca600b7fcfee7
nukdokplex <nukdokplex@nukdokplex.ru>
2025-12-17 19:14:22 +0500

    home-configurations/nukdokplex: yazi termfilechooser fix

--- home-configurations/nukdokplex/yazi/termfilechooser.nix
+++ home-configurations/nukdokplex/yazi/termfilechooser.nix
@@ -5,26 +5,35 @@
   ...
 }:
 {
-  xdg = lib.mkIf config.home.isDesktop {
-    portal = {
-      extraPortals = with pkgs; [ xdg-desktop-portal-termfilechooser ];
-      config = {
-        common = {
-          # use terminal file chooser
-          "org.freedesktop.impl.portal.FileChooser" = "termfilechooser";
-          # GTK portal gives you proper print dialogs.
-          "org.freedesktop.impl.portal.Print" = [ "gtk" ];
-          # GTK portal provides desktop settings that GTK apps query (fonts, themes, colour schemes).
-          "org.freedesktop.impl.portal.Settings" = [ "gtk" ];
+  config = lib.mkIf config.home.isDesktop {
+    xdg = {
+      portal = {
+        extraPortals = with pkgs; [ xdg-desktop-portal-termfilechooser ];
+        config = {
+          common = {
+            # use terminal file chooser
+            "org.freedesktop.impl.portal.FileChooser" = [ "termfilechooser" ];
+            # GTK portal gives you proper print dialogs.
+            "org.freedesktop.impl.portal.Print" = [ "gtk" ];
+            # GTK portal provides desktop settings that GTK apps query (fonts, themes, colour schemes).
+            "org.freedesktop.impl.portal.Settings" = [ "gtk" ];
+          };
         };
       };
+
+      configFile."xdg-desktop-portal-termfilechooser/config".text = ''
+        [filechooser]
+        cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
+        default_dir=$HOME
+        env=TERMCMD=${lib.getExe config.programs.foot.package} -a "foot-file-chooser"
+      '';
     };
 
-    configFile."xdg-desktop-portal-termfilechooser/config".text = ''
-      [filechooser]
-      cmd=${pkgs.xdg-desktop-portal-termfilechooser}/share/xdg-desktop-portal-termfilechooser/yazi-wrapper.sh
-      default_dir=$HOME
-      env=TERMCMD=${lib.getExe config.programs.foot.package} -a "foot-file-chooser"
-    '';
+    home.sessionVariables.GTK_USE_PORTAL = "1";
+
+    qt = {
+      qt5ctSettings.Appearance.standard_dialogs = "xdgdesktopportal";
+      qt6ctSettings.Appearance.standard_dialogs = "xdgdesktopportal";
+    };
   };
 }
