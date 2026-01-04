84389e0d2425114562b5e819d2a2474454db2611
nukdokplex <nukdokplex@nukdokplex.ru>
2026-01-04 09:14:23 +0500

    treewide: introduce deploy-rs integration

---
+++ deploy.nix
@@ -0,0 +1,44 @@
+{
+  inputs,
+  lib,
+  config,
+  ...
+}:
+let
+  inherit (inputs) deploy-rs;
+  deploySystem = "x86_64-linux"; # boo, deploy-rs, boo!!!
+
+  deployableSystems = [
+    "falhofnir"
+    "gladr"
+    "gler"
+    "holl"
+    "sleipnir"
+  ];
+
+  genDeployableSystem =
+    hostName:
+    let
+      host = inputs.self.outputs.nixosConfigurations.${hostName};
+    in
+    {
+      hostname = "${host.config.networking.hostName}.ndp.local";
+      profiles.system = {
+        user = "root";
+        path = deploy-rs.lib."${deploySystem}".activate.nixos host.config;
+      };
+    };
+in
+{
+  flake.deploy = {
+    nodes = lib.recursiveUpdate (lib.genAttrs deployableSystems genDeployableSystem) {
+      # custom config goes here
+    };
+  };
+
+  perSystem =
+    { system, ... }:
+    {
+      checks = deploy-rs.lib."${system}".deployChecks config.flake.deploy;
+    };
+}
