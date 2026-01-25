128107d16c336db65bb42abdfed7a5e237b7659b
nukdokplex <nukdokplex@nukdokplex.ru>
2026-01-26 04:23:42 +0500

    nixos-configurations/gladr: replace pipewire with pulseaudio

---
+++ nixos-configurations/gladr/pulseaudio.nix
@@ -0,0 +1,11 @@
+{ lib, ... }:
+{
+  services.pipewire.enable = lib.mkForce false;
+
+  services.pulseaudio = {
+    enable = true;
+    support32Bit = true;
+  };
+
+  nixpkgs.config.pulseaudio = true;
+}
